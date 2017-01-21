5b754835795ed7eb0c8d4e014d1178e376ef066f
purampurushotham <puram.purushotham@india.semanticbits.com>
2017-01-21 19:43:38 +0530

    View

--- software/MyShoppingCart/client/app/modules/views/view.controller.js
+++ software/MyShoppingCart/client/app/modules/views/view.controller.js
@@ -7,27 +7,50 @@
         .controller("viewCtrl",viewCtrl);
     viewCtrl.$inject=['$stateParams','$rootScope']
     function viewCtrl($stateParams,$rootScope) {
-         var vm=this;
-         vm.id=$stateParams.id;
-         var t=vm.id;
-         vm.vp={};
+        var vm=this;
+        vm.similarProducts=[];
+        vm.id=$stateParams.id;
+        var t=vm.id;
+        vm.vp={};
         vm.ele=$rootScope.products;
-         var data=vm.ele.length;
-          for (var i = 0; i < data; i++){
+        var data=vm.ele.length;
+        for (var i = 0; i < data; i++){
             var ele = vm.ele[i];
             console.log(ele);
             for (var key in ele){
                 if(t === ele[key]){
-                    console.log("matchfound"+ele[key])
-                vm.vp=ele;
+                    console.log("matchfound"+ele[key]);
+                    vm.vp=ele;
                     break;
                 }
 
             }
 
         }
-        console.log(vm.vp);
-  //       console.log($stateParams.id);
+        for(var i=0;i<data;i++){
+            var s=vm.ele[i];
+                if(vm.vp.subType === s["subType"]){
+                    console.log("matchfound in similar"+s["subType"]);
+                    vm.similarProducts.push(s);
+                    console.log(vm.similarProducts);
 
-     }
+            }
+        }
+    }
+
+
+    //       console.log($stateParams.id);
+
+    /* function similarProducts(similarPro,$rootScope){
+
+     console.log(similarPro);
+     console.log($rootScope.products);
+     var vm=this;
+     vm.similar={};
+     for (var i = 0; i < data; i++){
+     var ele = vm.ele[i];
+     console.log(ele);
+     for (var key in ele){
+     console.log("in similar  products");
+     }*/
 })();
