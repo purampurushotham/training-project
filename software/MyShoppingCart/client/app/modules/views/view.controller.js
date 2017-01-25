b20176b95e91f91c60d953b0554a4c7a05ba6ac1
purampurushotham <puram.purushotham@india.semanticbits.com>
2017-01-25 16:26:16 +0530

    added pagination

--- software/MyShoppingCart/client/app/modules/views/view.controller.js
+++ software/MyShoppingCart/client/app/modules/views/view.controller.js
@@ -17,7 +17,7 @@
         function getId() {
             angular.forEach($rootScope.products, function (product, index) {
                 if (vm.id === product.id) {
-                    console.log("matchfound" + vm.id);
+                 /*   console.log("matchfound" + vm.id);*/
                     vm.vp = product;
                 }
             });
@@ -25,11 +25,13 @@
         function similarProduct() {
             //ierate tolist to get brand and subtype
             angular.forEach($rootScope.products, function (similarProduct, index) {
-                if (vm.vp.brand === similarProduct.brand && vm.vp.subType === similarProduct.subType && vm.vp != similarProduct) {
+                //vm.vp.brand === similarProduct.brand &&
+                if ( vm.vp.subType === similarProduct.subType) {
+                    console.log(vm.vp.subType);
                     vm.similarProducts.push(similarProduct);
                     console.log(vm.similarProducts);
                 }
-                console.log("in similarProduct")
+                //console.log("in similarProduct")
             });
         }
 
@@ -45,7 +47,7 @@
                     ceil:30000,
                     step: 1000,
                     /*minRange: 1000,
-                    maxRange: 100000,*/
+                     maxRange: 100000,*/
                     onChange: onSliderChange,
                     translate: function(value, sliderId, label) {
                         switch (label) {
@@ -60,34 +62,49 @@
                 }
             };
             function onSliderChange() {
-                console.log("in slide change")
+              /*  console.log("in slide change")*/
                 vm.similarProducts = [];
                 angular.forEach(vm.copySimilarProds, function (eachProd, index){
                     if(eachProd.price <= vm.slider.maxValue && eachProd.price >= vm.slider.minValue) {
                         vm.similarProducts.push(eachProd);
-                        console.log(  vm.similarProducts);
+                        /*console.log(vm.similarProducts);*/
                     }
                 });
 
             }
 
         }
-        function userDefinedPagination(){
-            vm.currentPage=1;
-            vm.pageCount=5;
-            vm.pageLimit=5;
-            vm.pages=[];
-            vm.pageButtonDisabled = function(data) {
+        function userDefinedPagination() {
+            vm.currentPage = 0;
+            vm.totalPages = 0;
+            vm.pageSize = 3;
+            vm.pagedData = angular.copy(vm.similarProducts);
+            vm.similarProducts=[];
+            vm.pageButtonDisabled = function (data) {
                 if (data == -1) {
+                    console.log("in pageButton");
                     return vm.currentPage == 0;
                 }
-                return vm.currentPage >= dataFactory.data.length/vm .pageSize - 1;
+                return vm.currentPage >= vm.pagedData.length / vm.pageSize - 1;
+            }   
+            vm.paginate = function (nextPrevMultiplier) {
+                vm.currentPage += (nextPrevMultiplier * 1);
+                vm.similarProducts = vm.pagedData.slice(vm.currentPage * vm.pageSize);
+                console.log("in paginate");
             }
+
+            function selectedPage() {
+                vm.totalPages = Math.ceil(vm.pagedData.length / vm.pageSize);
+                vm.similarProducts = vm.pagedData;
+            }
+            selectedPage();
         }
-            //calling getId function
+        //calling all methods
         getId();
         similarProduct();
         sliderFeature();
+        userDefinedPagination();
+
     }
 
 })();
