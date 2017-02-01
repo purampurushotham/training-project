51200dab1e5ec3036e2b5211d732d5ab28b334a6
purampurushotham <puram.purushotham@india.semanticbits.com>
2017-02-01 17:12:27 +0530

    added header ,footer,home components with other files

---
+++ software/MyShoppingCart/client/app/modules/similarProducts/similarproducts.component.js
@@ -0,0 +1,28 @@
+/**
+ * Created by purushotham on 01-02-2017.
+ */
+(function () {
+    'use strict'
+    angular.module('MSC.similarProducts')
+        .component('similarProductsComponent',{
+            templateUrl : "app/partials/similarProduct.html",
+            controller : similarProduct,
+            controllerAs : "sc",
+            bindings:{
+
+            }
+        });
+    function similarProduct() {
+        var vm=this;
+        //ierate tolist to get brand and subtype
+        angular.forEach($rootScope.products, function (similarProduct, index) {
+            //vm.vp.brand === similarProduct.brand &&
+            if ( vm.vp.subType === similarProduct.subType) {
+                console.log(vm.vp.subType);
+                vm.similarProducts.push(similarProduct);
+                console.log(vm.similarProducts);
+            }
+            //console.log("in similarProduct")
+        });
+    }
+}());
